564769134c27042a7dc5d01159df4771a8362df6
DHV Developer <sahilwasalwar@gmail.com>
2026-06-15 15:16:34 +0530

    login registration has been done

---
+++ my-react-app/frontend/src/pages/AddProduct.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const [product,setProduct] = useState({
+    name:"",
+    price:"",
+    company:"",
+    expiry:"",
+    description:"",
+    stock:""
+});
+function HandleChange(event) {
+    const {name, value} = event.target;
+    setProduct({
+        ...product,[name]:value
+    });
+   return (
+    <div>
+        <input
+  type="text"
+  name="name"
+  placeholder="Medicine Name"
+  value={product.name}
+  onChange={handleChange}
+/>
+
+<input
+  type="number"
+  name="price"
+  placeholder="Price"
+  value={product.price}
+  onChange={handleChange}
+/>
+
+<input
+  type="text"
+  name="company"
+  placeholder="Company"
+  value={product.company}
+  onChange={handleChange}
+/>
+
+<input
+  type="text"
+  name="expiry"
+  placeholder="Expiry"
+  value={product.expiry}
+  onChange={handleChange}
+/>
+
+<input
+  type="number"
+  name="stock"
+  placeholder="Stock"
+  value={product.stock}
+  onChange={handleChange}
+/>
+
+<textarea
+  name="description"
+  placeholder="Description"
+  value={product.description}
+  onChange={handleChange}
+/>
+     
+    </div>
+   )
+}
