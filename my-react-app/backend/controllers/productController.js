6f320e58ec90765531dd4b90e8f1421081dedc3d
DHV Developer <sahilwasalwar@gmail.com>
2026-06-15 15:21:07 +0530

    login registration has been done

--- my-react-app/backend/controllers/productController.js
+++ my-react-app/backend/controllers/productController.js
@@ -22,6 +22,17 @@ const getProducts = async (
 
 };
 
+const createProduct =async (req,res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+    } catch(error){
+        res.status(500).json({
+            message:error.message
+        });
+    }
+}
+
 module.exports = {
-  getProducts
+  getProducts,createProduct
 };
