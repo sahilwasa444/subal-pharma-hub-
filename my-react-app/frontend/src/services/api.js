ee76d540a929616f1622040301cbd8b392e5a061
DHV Developer <sahilwasalwar@gmail.com>
2026-06-20 15:44:01 +0530

    Production API URL

--- my-react-app/frontend/src/services/api.js
+++ my-react-app/frontend/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
 const api = axios.create({
-    baseURL :"http://localhost:5000/api"
+  baseURL:
+    "https://subal-pharmahubat12.onrender.com/api"
 });
 export default api;
